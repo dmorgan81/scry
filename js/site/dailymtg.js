b39b853fa09473a8be1280c3abbe268a1d57213a
David Morgan <dmorgan81@gmail.com>
2013-10-22 10:02:11 -0400

    Change dailymtg.com query
    Instead of using the default, instead grab the keyname attribute and
    tweak it.

--- js/site/dailymtg.js
+++ js/site/dailymtg.js
@@ -1,5 +1,11 @@
 (function($, undefined) {
 
-    $('a.nodec').scry().removeAttr('onmouseover');
+    $('a.nodec').scry({
+        query : function() {
+            return $(this).attr('keyvalue').replace(/_|\[/g, function(m) {
+                return m === '_' ? ' ' : '\'';
+            });
+        }
+    }).removeAttr('onmouseover');
 
 })(jQuery);
